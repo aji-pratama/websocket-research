49fafbb94d77581a0407dfd865d000f20b78eace
Aji Pratama <atamasite@gmail.com>
2021-05-27 14:20:54 +0700

    Add xterm js

--- app/views.py
+++ app/views.py
@@ -5,6 +5,10 @@ def index(request):
     return render(request, 'app/index.html', {})
 
 
+def terminal(request):
+    return render(request, 'app/terminal.html', {})
+
+
 def room(request, room_name):
     return render(request, 'app/room.html', {
         'room_name': room_name
